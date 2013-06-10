a227131878814eba137236a0d8dfd9ef731123ac
Tim Chen <chengt@gmail.com>
2013-06-10 15:40:47 -0500

    added multicast

--- pzmq/client.py
+++ pzmq/client.py
@@ -2,7 +2,9 @@ import zmq
 from zmq_strategy import ZMQStrategy
 from zmq_strategy import client_strategy
 
-socket = ZMQStrategy(client_strategy).connect('tcp://127.0.0.1:10001')
+# pgm can only be used as pub/sub
+socket = ZMQStrategy(client_strategy).connect('epgm://239.255.0.1:10001', zmq.SUB)
+#socket = ZMQStrategy(client_strategy).connect('tcp://127.0.0.1:10001')
 
 def echo(msg):
     socket.send(msg, copy=False)
@@ -10,3 +12,11 @@ def echo(msg):
 
     server_response = socket.recv(copy=False)
     print "Client received %s" % server_response
+
+def listen():
+    # for this example listen to all topics
+    socket.setsockopt(zmq.SUBSCRIBE, '')
+
+    while True:
+        topic, msg = socket.recv_multipart()
+        print 'Client received topic: %s, msg:%s' % (topic, msg)
--- pzmq/server.py
+++ pzmq/server.py
@@ -1,12 +1,24 @@
 import zmq
 from zmq_strategy import ZMQStrategy
 from zmq_strategy import server_strategy
+from datetime import datetime
+from time import sleep
 
-socket = ZMQStrategy(server_strategy).connect('tcp://127.0.0.1:10001')
+# pgm can only be used as pub/sub
+socket = ZMQStrategy(server_strategy).connect('epgm://239.255.0.1:10001', zmq.PUB)
+# socket = ZMQStrategy(server_strategy).connect('tcp://127.0.0.1:10001', zmq.REP)
 
 def start():
     while True:
         msg = socket.recv(copy=False)
         print "Server received %s" % msg
+        socket.send_multipart(["time", msg])
+        print "Server sent %s" % msg
+
+def multicast_start():
+    while True:
+	time = datetime.now()
+        msg = "The time is now %s" % time
         socket.send(msg)
         print "Server sent %s" % msg
+        sleep(1)
